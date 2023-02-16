2d2cece3f3dad5cc6883fc1fe400979ece19cf8c
Florian Wagner <florianwagner.bremen@googlemail.com>
2023-02-16 14:59:07 +0100

    performance tests

--- performance-tests/framework.sh
+++ performance-tests/framework.sh
@@ -7,15 +7,15 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 rm -rf output.log
 
-RUN_NAMESPACE=${1:"vector_add"}
-RUN_PROGRAMM=${2:"vector_add_rtx3090_16_1024.out"}
+export RUN_NAMESPACE=${1:"vector_add"}
+export RUN_PROGRAMM=${2:"vector_add_rtx3090_16_1024.out"}
 
 mkdir -p "${SCRIPT_DIR}/${RUN_NAMESPACE}"
 
-for i in {1..500}
+for i in {1..50}
 do 
 { time {
-        echo "./${SCRIPT_DIR}/${RUN_NAMESPACE}/${RUN_PROGRAMM}"
+        echo ".${SCRIPT_DIR}/$RUN_NAMESPACE/$RUN_PROGRAMM"
     } } 2>> output.log
 done
 
