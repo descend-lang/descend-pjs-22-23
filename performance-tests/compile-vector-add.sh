b1c317a716bbe07add347ea4ec46470091f4c24b
Florian Wagner <florianwagner.bremen@googlemail.com>
2023-02-16 15:34:31 +0100

    performance tests#

--- performance-tests/compile-vector-add.sh
+++ performance-tests/compile-vector-add.sh
@@ -9,9 +9,9 @@ module load foss/2021a
 
 export PLAT="rtx3090"
 
-for i in {1..4}
+for i in {1..6}
 do 
-    for j in {1..4}
+    for j in {1..2}
     do 
         # wg=$(( i*16 ))
         wg=$(python3 -c "print(2**(3+$i))")
@@ -19,7 +19,7 @@ do
         echo "wg: $wg, th: $th"
         sed -i "s/WG XX/WG $wg/g" vector-add.cu
         sed -i "s/THREADS XX/THREADS $th/g" vector-add.cu
-        nvcc vector-add.cu -o vector_add_${PLAT}_${wg}_${th}.out --extended-lambda
+        nvcc ./runs/vector_add/vector-add.cu -o ./runs/vector_add/vector_add_${PLAT}_${wg}_${th}.out --extended-lambda
         sed -i "s/WG $wg/WG XX/g" vector-add.cu
         sed -i "s/THREADS $th/THREADS XX/g" vector-add.cu
         echo "successfully compiled for wg: $wg and th: $th"
---
+++ performance-tests/runs/vector_add/vector-add.cu
@@ -0,0 +1,36 @@
+#include "descend.cuh"
+
+#define WG XX
+#define THREADS XX
+
+template<std::size_t n>
+auto inplace_vector_add(
+        descend::i32 * const ha_array,
+        const descend::i32 * const hb_array
+) -> void {
+    const auto gpu = descend::gpu_device(0);
+    auto a_array = descend::gpu_alloc_copy<descend::array<descend::i32, n>>(&gpu, &*ha_array);
+    const auto b_array = descend::gpu_alloc_copy<descend::array<descend::i32, n>>(&gpu, &*hb_array);
+    descend::exec<WG, THREADS>(&gpu, [] __device__ (
+            descend::i32 * const p0,
+            const descend::i32 * const p1) -> void {
+        p0[blockIdx.x * THREADS + threadIdx.x] = p0[blockIdx.x * THREADS + threadIdx.x] + p1[blockIdx.x * THREADS + threadIdx.x];
+        __syncthreads();
+    }, &a_array, &b_array);
+    descend::copy_to_host<descend::array<descend::i32, n>>(&a_array, ha_array);
+}
+
+
+auto main() -> int {
+    auto ha_array = descend::HeapBuffer<descend::array<descend::i32, WG*THREADS>>(descend::create_array<WG * THREADS, descend::i32>(0));
+    const auto hb_array = descend::HeapBuffer<descend::array<descend::i32, WG*THREADS>>(descend::create_array<WG * THREADS, descend::i32>(1));
+    inplace_vector_add<WG*THREADS>(&ha_array, &hb_array);
+
+    for (size_t i = 0; i < WG*THREADS; i++) {
+        if (ha_array[i] != 1) {
+            std::cout << "At i = " << i << "Wrong number. Found " << ha_array[i] << " instead of 1.";
+            exit(EXIT_FAILURE);
+        }
+    }
+    exit(EXIT_SUCCESS);
+}
