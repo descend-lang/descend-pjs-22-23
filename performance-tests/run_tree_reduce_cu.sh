a394c1bf4cabb7a80fcfb9669e03122e681bef8f
Florian Wagner <florianwagner.bremen@googlemail.com>
2023-02-25 16:39:23 +0100

    went on

---
+++ performance-tests/run_tree_reduce_cu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+set -o pipefail
+# set -x
+
+# module load palma/2021a
+# module load CUDA
+# module load foss/2021a
+
+export PROG="tree_reduce_cu"
+export PLAT="rtx4000"
+
+for i in {1..2}
+do 
+    for j in {1..2}
+    do 
+        wg=$(python3 -c "print(2**(3+$i))")
+        th=$(python3 -c "print(2**(6+$j))")
+        ./framework.sh $PROG ${PROG}_${PLAT}_${wg}_${th}.out
+    done
+done
