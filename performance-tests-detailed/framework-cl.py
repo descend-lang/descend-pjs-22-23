a1318f2f549c757298e0c5fbbf89ed24d6d9dd5c
Florian Wagner <florianwagner.bremen@googlemail.com>
2023-02-26 16:53:54 +0100

    went on

---
+++ performance-tests-detailed/framework-cl.py
@@ -0,0 +1,14 @@
+import sys
+
+
+
+with open("output.log", "r") as f:
+    with open(f"{sys.argv[1]}.csv", "wt") as fout:
+        data = f.read()
+        # data = data.replace("kernel:reduce_shared_mem, Total kernel runtime","")
+        # data = data.replace(", ",",")
+        # data = data.replace(",\n\n","+")
+        # data = data.replace("+","\n")
+        # data = data[1:-2]
+        # data = "kernel,kernel\n" + data
+        fout.write(data)
