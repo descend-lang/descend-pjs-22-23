e2e59e281e02ed4bb6f65373c2c09908cd7519de
Florian Wagner <florianwagner.bremen@googlemail.com>
2023-02-26 18:07:01 +0100

    went on

--- performance-tests-detailed/framework-cu.py
+++ performance-tests-detailed/framework-cu.py
@@ -5,12 +5,12 @@ import sys
 with open("output.log", "r") as f:
     with open(f"{sys.argv[1]}.csv", "wt") as fout:
         data = f.read()
-        # data = data.replace("kernel:reduce_shared_mem, Total kernel runtime","")
+        data = data.replace("kernel:reduce, Total kernel runtime","")
         # data = data.replace("kernel:reduce,Total kernel runtime","")
         # data = data.replace("kernel:inplace_vector_add,Total kernel runtime","")
-        # data = data.replace(", ",",")
-        # data = data.replace(",\n\n","+")
-        # data = data.replace("+","\n")
-        # data = data[1:-2]
+        data = data.replace(", ",",")
+        data = data.replace(",\n\n","+")
+        data = data.replace("+","\n")
+        data = data[1:-2]
         # data = "kernel_cu,kernel_cu\n" + data
         fout.write(data)
