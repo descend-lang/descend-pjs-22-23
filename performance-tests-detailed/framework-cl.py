90902e2999a77bbb7d08729f841b476b3d05f42a
Florian Wagner <florianwagner.bremen@googlemail.com>
2023-02-26 16:58:40 +0100

    went on

--- performance-tests-detailed/framework-cl.py
+++ performance-tests-detailed/framework-cl.py
@@ -5,10 +5,10 @@ import sys
 with open("output.log", "r") as f:
     with open(f"{sys.argv[1]}.csv", "wt") as fout:
         data = f.read()
-        # data = data.replace("kernel:reduce_shared_mem, Total kernel runtime","")
-        # data = data.replace(", ",",")
-        # data = data.replace(",\n\n","+")
-        # data = data.replace("+","\n")
-        # data = data[1:-2]
-        # data = "kernel,kernel\n" + data
+        out = ""
+        for line in data.split("\n"):
+            if(line.startswith("time<<<")):
+                out = out + line.split("time<<<")[1] + "\n"
+
+        data = "kernel\n" + data
         fout.write(data)
