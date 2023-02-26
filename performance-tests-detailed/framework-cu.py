fcc9811fbfce1f715853c1e4ff83826c7b363a52
Florian Wagner <florianwagner.bremen@googlemail.com>
2023-02-26 16:50:44 +0100

    went on

--- performance-tests-detailed/framework-cu.py
+++ performance-tests-detailed/framework-cu.py
@@ -9,14 +9,6 @@ with open("output.log", "r") as f:
         data = data.replace(", ",",")
         data = data.replace(",\n\n","+")
         data = data.replace("+","\n")
-        # data = data.replace("user","")
-        # data = data.replace("sys","")
-        # data = data.replace("\t","")
-
-        # data = data.replace("\n",",")
-
-        # data = data.replace("0m","")
-        # data = data.replace("s","")
         data = data[1:-2]
         data = "kernel,kernel\n" + data
         fout.write(data)
